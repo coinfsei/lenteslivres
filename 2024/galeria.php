8af29635d7e1802b7cb5dc80113400d66e13bd45
Israel Sales <israelsales@outlook.com>
2025-01-31 09:19:55 -0300

    Primeiro commit

---
+++ 2024/galeria.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Head -->
+    <?php include('./snippets/head.html'); ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="70">
+
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/navegacao-galeria.html'); ?>
+
+    <!-- Sessão Home -->
+    <?php include('./snippets/home.html'); ?>
+
+    <!--Galeria -->
+    <?php include('./snippets/galeria.html'); ?>
+
+    <!-- Sessão dos Patrocinadores -->
+    <?php include('./snippets/patrocinio.html'); ?>
+
+    <!-- Modal de Versões Anteriores -->
+    <?php include('./snippets/botao-flutuante.html'); ?>
+
+    <!-- Footer -->
+    <?php include('./snippets/rodape.html'); ?>
+
+    <!-- Chamada dos Scripts -->
+    <?php include('./snippets/scripts.html'); ?>
+</body>
+
+</html>
---
+++ 2024/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<!-- Head -->
+ <?php include('./snippets/head.html'); ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="70">
+
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/navegacao.html'); ?>
+
+    <!-- Sessão Home -->
+    <?php //include('./snippets/home.html'); ?>
+
+    <!-- Sessão Sobre -->
+    <?php include('./snippets/sobre.html'); ?>
+
+    <!-- Sessão Submissão -->
+    <?php //include('./snippets/submissao.html'); 
+    ?>
+
+    <!-- Sessão de Programação -->
+    <?php //include('./snippets/programacao.html'); ?>
+
+    <!-- Modal de Download -->
+    <?php //include('./snippets/download.html'); ?>
+
+    <!-- Seção Palestrantes -->
+    <?php //include('./snippets/palestrantes.html'); ?>
+
+    <!-- Sessão dos Patrocinadores -->
+    <?php include('./snippets/patrocinio.html'); ?>
+
+    <!-- Modal de Versões Anteriores -->
+    <?php include('./snippets/botao-flutuante.html'); ?>
+
+    <!-- Footer -->
+    <?php include('./snippets/rodape.html'); ?>
+
+    <!-- Chamada dos Scripts -->
+    <?php include('./snippets/scripts.html'); ?>
+</body>
+
+</html>

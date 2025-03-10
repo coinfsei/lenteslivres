2447578371a3baad14061c193344cbbb6ace6136
- <->
2025-03-10 14:36:32 -0300

    Feat:Adição de pagian COMO ENVIAR

---
+++ 2024/tutorial-envio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<!-- Head -->
+ <?php include('./snippets/head.html'); ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="70">
+
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/navegacao-inscricao.html'); ?>
+
+    <!-- Sessão Banner -->
+    <?php include('./snippets/banner.html'); ?>
+
+    <section id="about" class="py-section">
+        <div class="container">
+            <h4>CRITERIOS PARA ARQUIVOS</h4>
+            <br>
+            <p>Para seguir com sua com seu cadastro sera preciso enviar alguns documento</p>
+            <p>Esses documentos são:</p>
+            <ul>
+                <li>Foto do documento de indentificação</li>
+                <li>Comprovante de residencia</li>
+                <li>Video de no maximo 3 minutos de duração</li>
+                <li>Indentificação do candidato e da proposta</li>
+            </ul>
+            <h5>Limite de envio</h5>
+            <p>Para envio de documentos o como JPG,JPEG,PNG,PDF:<br><strong>O limite de envio são de 20MB</strong></p>
+            <br>
+            <p>Para envio de documentos o como mp4,:<br><strong>O limite de envio são de 20MB</strong></p>
+            <h5></h5>
+        </div>
+    </section>
+
+    

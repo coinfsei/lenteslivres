9cfff8261a80fb5f1582cbce341fb18d57301536
- <->
2025-03-11 12:05:17 -0300

    Feat:adição de conteudo sobre pagina tutorial-envio.php

--- 2024/tutorial-envio.php
+++ 2024/tutorial-envio.php
@@ -33,16 +33,19 @@
             <p>Para envio de documentos o como MP4,MKV,WEBM,AVI,M4A:<br><strong>O limite de envio são de 500MB</strong></p>
             <br>
             <h5>Como Diminuir o tamanho do seu arquivo de Video</h5>
-            <p>Para Fazer a dimunicão do tamanho do video iremos recomenda o CapCut, um editor basico de edição de video<br>O CapCut possui tanto para a versão de celular quanto para</p>
+            <br>
+            <p>Para Fazer a dimunicão do tamanho do video iremos recomenda o CapCut, Pode ser utilizado outro editor de video mais utilizaremos o Capcut por ser mais pratico e agil</p>
+            <p>O CapCut possui tanto para a versão de celular quanto para desktop</p>
+            <p>Na versão de celular tem quer ser feita o download na sua loja de aplicativo</p>
+
+            
         </div>
     </section>
     <section class="py-section bg-light">
         <div class="container">
-        <h5>Como Diminuir o tamanho do seu arquivo de Video</h5>
-        <br>
-        <p>Para Fazer a dimunicão do tamanho do video iremos recomenda o CapCut, um editor basico de edição de video<br>O CapCut possui tanto para a versão de celular quanto online</p>
 
-        <h5>Como Utilizar o CapCut Online no computador e celular</h5>
+        <h5>Como Utilizar o CapCut Online no desktop</h5>
+        <br>
 
         <p>Pesquise em seu navegador, CapCut Online ou utilize esse link para ser enviado para a pagina <a href="https://www.capcut.com/pt-br/tools/online-video-editor" target="_blank">CapCut Online</a></p>
 
@@ -62,10 +65,63 @@
         <p>Selecione a opção <strong>Tela em branco</strong><br></p>
         <img src="./imgs/imagens/CapCut_selecao.png" alt="CapCut Home Page" class="img-fluid">
         <br>
-       
-        
+        <p>Agora na tela principal do CapCut, clique no botão de <strong>"+"</strong> para adiciona o seu video</p>
+        <img src="./imgs/imagens/CapCut_selecao_video.png" alt="CapCut Home Page" class="img-fluid">
+        <br>
+        <p>Com o video selecionado, clique no botão de <strong>Exportar</strong> para fazer a redução do arquivo</p>
+        <img src="./imgs/imagens/CapCut_exportacao.png" alt="CapCut Home Page" class="img-fluid">
+        <br>
+        <p>clique no botão de <strong>Baixar</strong> para fazer a redução do arquivo</p>
+        <img src="./imgs/imagens/CapCut_edicao_3.png" alt="CapCut Home Page" class="img-fluid">
+        <br>
+        <p>Nessa Aba, você pode escolher a resolução que você quer para o video </p><p>Recomendamos que utilize a resolução como <strong>1080p</strong> ou <strong>720p</strong></p><p>Logo em seguida clique em <strong>Exportar</strong></p>
+        <img src="./imgs/imagens/CapCut_edicao_4.png" alt="CapCut Home Page" class="img-fluid">
+        <br>
+        <p>Agora so clicar em<strong> Baixar</strong> e você tera seu video baixado e com o tamanho reduzido</p>
+        <img src="./imgs/imagens/CapCut_final.png" alt="CapCut Home Page" class="img-fluid">
+        <br>
+        </div>
+    </section>
+
+    <section id="about" class="py-section">
+        <div class="container">
+
+            <h5>Como Utilizar o CapCut no Android ou Iphone</h5>
+            <br>
+
+            <p>Tanto no Android quanto no Iphone, sera preciso fazer o donwload do aplicativo na loja de aplicativo no seu dispositivo</p>
+            <p>Va ate sua loja de aplicativos e busque por CapCut e clique em instalar</p>
+            <img src="./imgs/imagens/CapCut_mobile_download.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Ao iniciar o aplicativo clique em pular no top do aplicativo para pula a introdução<br></p>
+            <img src="./imgs/imagens/CapCut_mobile_skip.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Você sera levado a pagina inicial do aplicativo, procure agora clique em <strong>Novo projeto</strong> na tela de edição</p>
+            <img src="./imgs/imagens/CapCut_mobile_home.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Na area de seleção, clique para selecionar o video quer deseja editar</p>
+            <img src="./imgs/imagens/CapCut_mobile_selecao.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Pronto agora você esta dentro da pagina de edição do CapCut</p>
+            <img src="./imgs/imagens/CapCut_mobile_video.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <h5>Edição Opcional</h5>
+            <br>
+            <p>O CapCut No final do video adiciona uma cena de encerramento, que pode ser removida pelo usuario caso deseje</p>
+            <p>Caso não deseje, pode pular para o proximo passo</p>
+            <p>Para remover essa finalização, selecione a área final do seu video para removela</p>
+            <p>Depois clique no icone de <strong>lixeira</strong></p>
+            <img src="./imgs/imagens/CapCut_mobile_remove.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Para altera o tamnaho do seu video clique no em AI ultra HD</p>
+            <img src="./imgs/imagens/CapCut_mobile_exportacao.png" alt="CapCut mobile" class="img-fluid">
+            <br>
+            <p>Pronto agora você esta dentro da pagina de edição do CapCut</p>
+            <img src="./imgs/imagens/CapCut_mobile_video.png" alt="CapCut mobile" class="img-fluid">
+            <br>
 
         </div>
     </section>
+  
 
     
